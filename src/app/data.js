ba6add280416079903183ddea941fea265fdd0f0
StarLord7 <samuelnasa375@gmail.com>
2024-02-16 11:25:07 -0600

    guia 4

---
+++ src/app/data.js
@@ -0,0 +1,71 @@
+export const data = [
+    {
+        id: 1,
+        title: 'Cien años de soledad',
+        price: 100,
+        urlImage: 'https://images.penguinrandomhouse.com/cover/9780525562443',
+        quantity: 1
+    },
+    {
+        id: 2,
+        title: 'El señor de los anillos(Trilogía)',
+        price: 190,
+        urlImage: 'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+        quantity: 1
+    },
+    {
+        id: 3,
+        title: 'Cuentos de Barro',
+        price: 30,
+        urlImage: 'https://www.librosdelaballena.com/wp-content/uploads/2020/05/cuentos-barro-244x300.png',
+        quantity: 1
+    },
+    {
+        id: 4,
+        title: 'Tierra de Infancia',
+        price: 30,
+        urlImage: 'https://marketsvstg.blob.core.windows.net/marketsv/0011705_tierra-de-infancia_510.jpeg',
+        quantity: 1
+    }, {
+        id: 5,
+        title: 'Harry Potter Pack',
+        price: 390,
+        urlImage: 'https://contentv2.tap-commerce.com/cover/large/9789878000473_1.jpg?id_com=1113',
+        quantity: 1
+    },
+    {
+        id: 6,
+        title: 'El Boson de Higgs',
+        price: 25,
+        urlImage: 'https://www.mentesliberadas.com/wp-content/uploads/2019/04/boson-de-higgs-1024x742.jpg',
+        quantity: 1
+    },
+    {
+        id: 7,
+        title: 'Maze Runner',
+        price: 15,
+        urlImage: 'https://www.zigzag.cl/pub/media/catalog/product/cache/f40365484e36b02d74dc5711a111d955/9/7/9789876122672_1.jpg',
+        quantity: 1
+    },
+    {
+        id: 8,
+        title: 'Los Juegos del Hambre',
+        price: 12,
+        urlImage: 'https://www.mosca.com.uy/media/catalog/product/8/4/848887.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=700&width=700&canvas=700:700',
+        quantity: 1
+    },
+    {
+        id: 9,
+        title: 'COSMOS',
+        price: 30,
+        urlImage: 'https://mujeresconciencia.com/app/uploads/2020/08/cosmos-mundos-posibles_c367f24a_500x748.jpg',
+        quantity: 1
+    },
+    {
+        id: 10,
+        title: 'El Libro de la Fisica',
+        price: 30,
+        urlImage: 'https://www.elsotano.com/imagenes_grandes/9780241/978024150112.JPG',
+        quantity: 1
+    }
+];
